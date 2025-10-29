ca83dd3612e65fc34c5b88e9fd918dafac857eed
Asadulla56 <mdasadullahoque294@gmail.com>
2025-10-29 11:13:31 +0600

    create to the banner section

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -15,9 +15,9 @@ const Banner = () => {
           Level Up Your Gaming Experience
         </h2>
         <p className="max-w-[343px] font-medium text-gray-800/60">
-          From immersive sound to precise controls—everything you need to win
+          From immersive sound to precise controls everything you need to win
         </p>
-        <button className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-orange-600 rounded text-white">
+        <button className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-blue-500 rounded text-white">
           Buy now
           <Image className="group-hover:translate-x-1 transition" src={assets.arrow_icon_white} alt="arrow_icon_white" />
         </button>
